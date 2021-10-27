6448045ef967cac29fae24f8cd9454434f02512c
Artem <artem@Artems-MacBook-Pro.local>
2021-10-27 21:21:02 +0300

    Fixed renderRoute path in for child route

--- src/components/renderRoute.tsx
+++ src/components/renderRoute.tsx
@@ -3,25 +3,14 @@ import { Route, Switch } from "react-router";
 export const renderRoute = (parentRoutePath: string | null, route: any) => {
   const { children, component, path, exact } = route;
 
+  const routePath = parentRoutePath ? `${parentRoutePath}${path}` : path;
   if (!children.length) {
-    return (
-      <Route
-        key={path}
-        path={parentRoutePath ? `${parentRoutePath}${path}` : path}
-        exact={exact}
-        component={component}
-      />
-    );
+    return <Route key={routePath} path={routePath} exact={exact} component={component} />;
   } else {
     return (
       <Switch key="parent">
-        <Route
-          key={path}
-          path={parentRoutePath ? `${parentRoutePath}${path}` : path}
-          exact={exact}
-          component={component}
-        />
-        {children.map((childRoute: any) => renderRoute(path, childRoute))}
+        <Route key={path} path={routePath} exact={exact} component={component} />
+        {children.map((childRoute: any) => renderRoute(routePath, childRoute))}
       </Switch>
     );
   }
